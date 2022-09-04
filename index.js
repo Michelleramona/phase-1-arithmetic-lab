57ea8c17ab8d295ca5c5ac021f0d7dfb5503c93f
Michelle Vitello <michelleramona89@gmail.com>
2022-09-04 14:20:33 -0400

    commit tree 19c337329457a4e4e3be8644355af6e5cfc3a940

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 1
+let num2 = 62
+let multiply = (num1 * num2);
 
-// Write your code here
+let random = 5
+
+let num3 = 8
+let num4 = 2
+let mod = (num3/num4);
+
+let max = Math.max(18,19,20)
